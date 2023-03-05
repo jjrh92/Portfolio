198d23c2eca1b3c9789e09d57b7fd79f34f2d0a0
Julio Reyes <juliojesusreyes@gmail.com>
2023-03-05 15:14:08 -0300

    Comenzando con formulario

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 // Inicio
 
+let nombreyApellido = document.getElementById("nombreyApellido");
+let email = document.getElementById("email");
+let asunto = document.getElementById("asunto");
+let mensaje = document.getElementById("mensaje");
+let enviarformulario = document.getElementById("enviarformulario");
+
+
+
 // V-1.0.0
 // Fin
