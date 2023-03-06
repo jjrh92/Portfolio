d03b8ab5c104270972cebd94f8aa591589bc858c
Julio Reyes <juliojesusreyes@gmail.com>
2023-03-05 21:54:12 -0300

    Validacion formulario

--- main.js
+++ main.js
@@ -1,12 +1,51 @@
 // Inicio
 
-let nombreyApellido = document.getElementById("nombreyApellido");
-let email = document.getElementById("email");
-let asunto = document.getElementById("asunto");
-let mensaje = document.getElementById("mensaje");
-let enviarformulario = document.getElementById("enviarformulario");
+const nombreyApellido = document.getElementById ("nombreyApellido");
+const email = document.getElementById ("email");
+const asunto = document.getElementById ("asunto");
+const mensaje = document.getElementById("mensaje");
+const botonEnviarFormulario = document.getElementById ("botonEnviarFormulario");
+
+function revisarDatos () {
+
+    // Con esta Primera validacion evitamos que la pagina se actualice al enviar el formulario.
+    window.event.preventDefault()
+
+    // Validaciones de Nombre y Apellido 
+
+    if (nombreyApellido == "" || nombreyApellido.value.length > 50) {
+
+        alert ("Debe ingresar nombre y apellido valido");
+
+    }
+
+    // Validaciones de Correo Electronico 
+
+    else if (email == "") {
+
+        alert ("Debe ingresar correo electronico valido");
+
+    }
+
+    else if (email.indexOf ("@") == -1 || email.indexOf (".") == -1 ) {
+
+        alert ("Debe ingresar correo electronico valido");
+
+    }
+
+    // Validaciones de Asunto
+
+    else if (asunto == "" || asunto.length < 50) {
+
+        alert ("Debe ingresar un mensaje valido");
+    
+    }
 
 
+
+}
+
+botonEnviarFormulario.addEventListener ("click", revisarDatos);
 
 // V-1.0.0
 // Fin
